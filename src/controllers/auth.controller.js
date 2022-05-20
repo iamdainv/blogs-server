1e90fe7b704ff4e5f9b1c968c9a36188658f85b2
Sotatek-DaiNguyen2 <dai.nguyen2@sotatek.com>
2022-05-20 09:39:28 +0700

    fake token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,6 +2,15 @@ const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
 const { authService, userService, tokenService, emailService, phoneService } = require('../services');
 
+// const createSpecToken = async () => {
+//   const user = await authService.loginUserWithPhoneNumber('0123456789');
+//   const tokens = await tokenService.generateAuthTokens(user);
+
+//   console.log(tokens);
+// };
+
+// createSpecToken();
+
 const register = catchAsync(async (req, res) => {
   const user = await userService.createUser(req.body);
   const tokens = await tokenService.generateAuthTokens(user);
